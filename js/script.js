75bf76d687d1d6eb0301cc8a5490fb138bf308e6
nobles1 <blakenoblesyear@gmail.com>
2020-12-17 16:05:16 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1100},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1000},"pages":[{"id":"page-home-","displayName":"Home ","link":{"linkType":"LinkTypePage","href":"#!page-home-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-event","displayName":"Event","link":{"linkType":"LinkTypePage","href":"#!page-event"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product-details","displayName":"Product Details","link":{"linkType":"LinkTypePage","href":"#!page-product-details"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
+            });
+
+            /*
+                animation widget from here: https://github.com/silexlabs/Silex/issues/443
+            */
+
+            window.sr = ScrollReveal({
+                distance: '100px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+
+        })
+    
