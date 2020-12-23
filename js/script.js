2c313c76c3225cff8055918b45c771f7cc58b655
nobles1 <blakenoblesyear@gmail.com>
2020-12-23 07:52:31 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1074},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-products","displayName":"Products","link":{"linkType":"LinkTypePage","href":"#!page-products"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about","displayName":"About","link":{"linkType":"LinkTypePage","href":"#!page-about"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":963},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-music-page","displayName":"Music Page","link":{"linkType":"LinkTypePage","href":"#!page-music-page"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+        $(document).ready(function() {
+        	$('.fancybox').each(function () {
+        	    var href = this.getAttribute('data-silex-href') || this.getAttribute('href');
+        	    if (this.getAttribute('data-silex-href')) {
+            	    this.removeAttribute('data-silex-href');
+            	    // for the fancybox script
+            	    this.setAttribute('href', href);
+            	    // prevent silex from opening link
+            	    $(this).off('click');
+        	    }
+        	    // this is a galery
+        	    this.setAttribute('data-fancybox-group', 'group1');
+        	});
+        	// create the fancybox
+        	$('.fancybox').fancybox();
+        });
+    
