74564f4b38eb7388a3a4764d5b774b4862d15ff2
nobles1 <blakenoblesyear@gmail.com>
2020-12-24 09:42:32 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1000},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-music-page","displayName":"Music Page","link":{"linkType":"LinkTypePage","href":"#!page-music-page"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
-
-/*
- * animation widget from here: https://github.com/silexlabs/Silex/issues/443
- */
-$(function() {
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if(!isMobile) {
-        window.sr = ScrollReveal({
-            distance: '100px'
-        });
-        sr.reveal('.from-left', { origin:  'left'});
-        sr.reveal('.from-right', { origin:  'right'});
-        sr.reveal('.from-top', { origin:  'top'});
-        sr.reveal('.from-bottom', { origin:  'bottom'});
-    }
-})
-    
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-music-page","displayName":"Music Page","link":{"linkType":"LinkTypePage","href":"#!page-music-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-church-service","displayName":"Church Service","link":{"linkType":"LinkTypePage","href":"#!page-church-service"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-event","displayName":"Event","link":{"linkType":"LinkTypePage","href":"#!page-event"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
