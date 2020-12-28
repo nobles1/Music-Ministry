b80c1edec6cc5efde3db9853c5b65d1a0b4c8aa3
nobles1 <blakenoblesyear@gmail.com>
2020-12-28 07:29:35 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-music-page","displayName":"Music Page","link":{"linkType":"LinkTypePage","href":"#!page-music-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-church-service","displayName":"Church Service","link":{"linkType":"LinkTypePage","href":"#!page-church-service"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-event","displayName":"Event","link":{"linkType":"LinkTypePage","href":"#!page-event"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+
+/*
+ * active menu widget for Silex
+ * create an element which links to an anchor, e.g. an element with a link to #anchor1
+ * add the css class "anchor-link" to this element
+ * create an element which is the anchor, e.g. an element with the css class "anchor1"
+ * when the user clicks on the link, the scroll slides until the element is visible
+ * when the user slides and the element is visible, the link gets a css class "active-menu"
+ */
+$(function() {
+    var container = $('.anchor-container');
+    var containerTop = 0;
+    $(window).scroll(checkScroll);
+    checkScroll();
+    function checkScroll(){
+        var fromTop = $(this).scrollTop();
+        if(containerTop < fromTop) {
+           container.addClass('offscreen');
+        }
+        else {
+           container.removeClass('offscreen');
+        }
+    }
+});
